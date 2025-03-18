3183a6100d8ff0579c800067fccad901f9b813be
Ishika Casley <ishikacasley764@gmail.com>
2025-03-18 20:27:28 +0530

    Added winning highlight

--- components/Board.js
+++ components/Board.js
@@ -4,26 +4,22 @@ import React from 'react';
 import { Grid, Paper } from '@mui/material';
 import Square from './Square';
 
-const Board = ({ board, onMove }) => {
+const Board = ({ board, onMove, playerX, playerO, winningLine }) => {
   return (
-    <Paper
-      elevation={3}
-      sx={{
-        padding: 2,
-        margin: 'auto',
-        maxWidth: 320,
-        backgroundColor: '#ffffff',
-      }}
-    >
+    <Paper elevation={3} sx={{ padding: 2, margin: 'auto', maxWidth: 320 }}>
       <Grid container spacing={1}>
         {board.map((value, index) => (
           <Grid item xs={4} key={index}>
-            <Square value={value} onClick={() => onMove(index)} />
+            <Square
+              value={value}
+              displayValue={value}
+              onClick={() => onMove(index)}
+              isWinning={winningLine?.includes(index)}
+            />
           </Grid>
         ))}
       </Grid>
     </Paper>
   );
 };
-
 export default Board;
